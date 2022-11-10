1db0337c5b7cce7d2169a048c5c47afc9f67b5df
tarequrrahman <tarequrrahman@nextgenit.com>
2022-11-10 21:33:45 +0600

    installation intervention package for image

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Yajra\DataTables\DataTablesServiceProvider::class,  //using yajra database for lots of data
+        Intervention\Image\ImageServiceProvider::class,  //image resize package
 
         /*
          * Application Service Providers...
@@ -230,7 +231,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Image' => Intervention\Image\Facades\Image::class,
     ],
 
 ];
