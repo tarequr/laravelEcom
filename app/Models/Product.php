dd64e4db2d3f280ed97b1959afd35c3820ac853e
tarequrrahman <tarequrr8@gmail.com>
2022-12-11 22:00:28 +0600

    product relationship with others table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,4 +10,29 @@ class Product extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function subCategory()
+    {
+        return $this->belongsTo(SubCategory::class);
+    }
+
+    public function childCategory()
+    {
+        return $this->belongsTo(ChildCategory::class,'childcategory_id','id');
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class,'brand_id','id');
+    }
+
+    public function pickupPoint()
+    {
+        return $this->belongsTo(PickupPoint::class,'pickup_id','id');
+    }
 }
