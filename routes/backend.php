024bc66db7e9345716ec0eff6632fee053859422
tarequrrahman <tarequrr8@gmail.com>
2022-12-20 23:13:33 +0600

    auth check

--- routes/backend.php
+++ routes/backend.php
@@ -27,37 +27,40 @@ use App\Http\Controllers\BackEnd\WarehouseController;
 
 Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/home',[DashboardController::class,'index'])->name('home');
-// Route::resource('categories',CategoryController::class)->except('create','edit','show');
-Route::resource('category',CategoryController::class);
-Route::resource('subcategory',SubCategoryController::class);
-Route::resource('childcategory',ChildCategoryController::class);
-Route::resource('brand', BrandController::class);
-Route::resource('product', ProductController::class);
-Route::resource('warehouse', WarehouseController::class);
-Route::resource('coupon', CouponController::class);
-Route::resource('pickup', PickupController::class);
+Route::group(['middleware'=>'auth'],function(){
+    // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/home',[DashboardController::class,'index'])->name('home');
+    // Route::resource('categories',CategoryController::class)->except('create','edit','show');
+    Route::resource('category',CategoryController::class);
+    Route::resource('subcategory',SubCategoryController::class);
+    Route::resource('childcategory',ChildCategoryController::class);
+    Route::resource('brand', BrandController::class);
+    Route::resource('product', ProductController::class);
+    Route::resource('warehouse', WarehouseController::class);
+    Route::resource('coupon', CouponController::class);
+    Route::resource('pickup', PickupController::class);
 
-Route::get('not-featured/{id}', [ProductController::class,'notFeatured']);
-Route::get('active-featured/{id}', [ProductController::class,'activeFeatured']);
+    Route::get('not-featured/{id}', [ProductController::class,'notFeatured']);
+    Route::get('active-featured/{id}', [ProductController::class,'activeFeatured']);
 
-Route::get('not-toadydeal/{id}', [ProductController::class,'notToadydeal']);
-Route::get('active-toadydeal/{id}', [ProductController::class,'activeToadydeal']);
+    Route::get('not-toadydeal/{id}', [ProductController::class,'notToadydeal']);
+    Route::get('active-toadydeal/{id}', [ProductController::class,'activeToadydeal']);
 
-Route::get('not-status/{id}', [ProductController::class,'notStatus']);
-Route::get('active-status/{id}', [ProductController::class,'activeStatus']);
+    Route::get('not-status/{id}', [ProductController::class,'notStatus']);
+    Route::get('active-status/{id}', [ProductController::class,'activeStatus']);
 
-/* ~~~~~~~~~~~~GLOBAL ROUTE~~~~~~~~~~~~~~~ */
-Route::get('get-child-category/{id}',[CategoryController::class,'getChildCategory']);
+    /* ~~~~~~~~~~~~GLOBAL ROUTE~~~~~~~~~~~~~~~ */
+    Route::get('get-child-category/{id}',[CategoryController::class,'getChildCategory']);
 
-/* ~~~~~~~~~~~~SETTING~~~~~~~~~~~~~~~ */
-Route::group(['prefix' => 'setting'], function(){
-    Route::get('seo',[SettingController::class,'seoIndex'])->name('setting.seo');
-    Route::put('seo/{id}/update',[SettingController::class,'seoUpdate'])->name('setting.seo.update');
-    Route::get('smtp',[SettingController::class,'smtpIndex'])->name('setting.smtp');
-    Route::put('smtp/{id}/update',[SettingController::class,'smtpUpdate'])->name('setting.smtp.update');
-    Route::get('website',[SettingController::class,'websiteIndex'])->name('setting.website');
-    Route::put('website/{id}/update',[SettingController::class,'websiteUpdate'])->name('setting.website.update');
-    Route::resource('page', PageController::class);
+    /* ~~~~~~~~~~~~SETTING~~~~~~~~~~~~~~~ */
+    Route::group(['prefix' => 'setting'], function(){
+        Route::get('seo',[SettingController::class,'seoIndex'])->name('setting.seo');
+        Route::put('seo/{id}/update',[SettingController::class,'seoUpdate'])->name('setting.seo.update');
+        Route::get('smtp',[SettingController::class,'smtpIndex'])->name('setting.smtp');
+        Route::put('smtp/{id}/update',[SettingController::class,'smtpUpdate'])->name('setting.smtp.update');
+        Route::get('website',[SettingController::class,'websiteIndex'])->name('setting.website');
+        Route::put('website/{id}/update',[SettingController::class,'websiteUpdate'])->name('setting.website.update');
+        Route::resource('page', PageController::class);
+    });
+
 });
