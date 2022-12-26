03fefbdd5fae5995949d3c6daf48ff1823dd11f5
tarequr <tarequrr8@gmail.com>
2022-12-26 20:44:55 +0600

    frontend templating

--- app/Http/Controllers/FrontEnd/HomeController.php
+++ app/Http/Controllers/FrontEnd/HomeController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    // public function index(Type $var = null)
-    // {
-    //     // return view('');
-    // }
+    public function index(Type $var = null)
+    {
+        return view('frontend.home.index');
+    }
 }
