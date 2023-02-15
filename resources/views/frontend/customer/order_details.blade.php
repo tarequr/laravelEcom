473dabaf5a8e4f527f7157d3198a708d16c8891d
tarequr <tarequrr8@gmail.com>
2023-02-15 21:07:43 +0600

    showing order details

--- resources/views/frontend/customer/order_details.blade.php
+++ resources/views/frontend/customer/order_details.blade.php
@@ -21,16 +21,29 @@
                 <br>
                 <div class="card">
                     <div class="card-body">
-                        Name:
-                        Phone:
-                        Order ID:
-                        Status:
-                        Date:
-                        Subtotal:
-                        Total:
+                        Name: {{ $order->c_name }} <br>
+                        Phone: {{ $order->c_phone }} <br>
+                        Order ID: {{ $order->order_id }} <br>
+                        Status: @if ($order->status == 0)
+                                    <span class="badge badge-danger">Order Pending</span>
+                                @elseif ($order->status == 1)
+                                    <span class="badge badge-info">Order Received</span>
+                                @elseif ($order->status == 2)
+                                    <span class="badge badge-primary">Order Shipping</span>
+                                @elseif ($order->status == 3)
+                                    <span class="badge badge-success">Order Done</span>
+                                @elseif ($order->status == 4)
+                                    <span class="badge badge-warning">Order Return</span>
+                                @elseif ($order->status == 5)
+                                    <span class="badge badge-danger">Order Cancle</span>
+                                @endif
+                                <br>
+                        Date: {{ date('d-M-Y', strtotime($order->date)) }} <br>
+                        Subtotal: {{ $order->subtotal }}{{$setting->currency}} <br>
+                        Total: {{ $order->total }}{{$setting->currency}} <br>
                     </div>
                 </div>
-                <h4>My Order Details</h4>
+                <h4 class="mt-3">My Order Details</h4>
                 <div class="table-responsive">
                     <table class="table table-borderd table-striped">
                         <thead>
@@ -45,34 +58,17 @@
                             </tr>
                         </thead>
                         <tbody>
-                            {{-- @foreach ($order_details as $order_detail)
+                            @foreach ($order_details as $order_detail)
                             <tr>
                                 <th scope="row">{{ $loop->iteration }}</th>
-                                <td>{{ $order->order_id }}</td>
-                                <td>{{ date('d-M-Y', strtotime($order->date)) }}</td>
-                                <td>{{ ucwords(str_replace("_"," ",$order->payment_type)) }}</td>
-                                <td>
-                                    @if ($order->status == 0)
-                                        <span class="badge badge-danger">Order Pending</span>
-                                    @elseif ($order->status == 1)
-                                        <span class="badge badge-info">Order Received</span>
-                                    @elseif ($order->status == 2)
-                                        <span class="badge badge-primary">Order Shipping</span>
-                                    @elseif ($order->status == 3)
-                                        <span class="badge badge-success">Order Done</span>
-                                    @elseif ($order->status == 4)
-                                        <span class="badge badge-warning">Order Return</span>
-                                    @elseif ($order->status == 5)
-                                        <span class="badge badge-danger">Order Cancle</span>
-                                    @endif
-                                </td>
-                                <td>
-                                    <a href="{{ route('view.order',$order->id) }}" class="btn btn-sm btn-info" title="Order Details">
-                                        <i class="fa fa-eye"></i>
-                                    </a>
-                                </td>
+                                <td>{{ $order_detail->product_name }}</td>
+                                <td>{{ $order_detail->color }}</td>
+                                <td>{{ $order_detail->size }}</td>
+                                <td>{{ $order_detail->quantity }}</td>
+                                <td>{{ $order_detail->single_price }}{{$setting->currency}}</td>
+                                <td>{{ $order_detail->subtotal_price }}{{$setting->currency}}</td>
                             </tr>
-                            @endforeach --}}
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
