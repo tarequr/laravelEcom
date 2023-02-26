26108600d6bc8367a58bd285ca787d9d8f3625ec
tarequr <tarequrr8@gmail.com>
2023-02-26 22:39:37 +0600

    blog migration created

---
+++ database/migrations/2023_02_26_163501_create_blogs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('title')->unique();
+            $table->string('slug');
+            $table->date('date')->nullable();
+            $table->longText('description')->nullable();
+            $table->string('thumbnail')->nullable();
+            $table->string('tag')->nullable();
+            $table->boolean('status')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
