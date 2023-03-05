dd12e7bee5e11ed98feca78b6921c34db9d82db8
tarequr <tarequrr8@gmail.com>
2023-03-05 22:20:12 +0600

    created campaign product migration

---
+++ database/migrations/2023_03_05_161513_create_campaign_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampaignProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campaign_products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('campaign_id')->constrained('campaigns')->onDelete('cascade');
+            $table->foreignId('product_id')->constrained('campaigns')->onDelete('cascade');
+            $table->string('price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campaign_products');
+    }
+}
